10af99eb855c8d3c5d76179b18beb49fee548bbd
Denish099 <gyldenish@gmail.com>
2025-04-25 16:58:07 +0530

    intialized auth routes

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express } from "express";
+import { config } from "dotenv";
+import authRoutes from "./routes/authRoutes";
+
+config();
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
