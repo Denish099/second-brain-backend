a7127de3e52be21a649e7eb68e9b8439f2fceeb2
Denish099 <gyldenish@gmail.com>
2025-04-27 10:21:57 +0530

    changed content model

--- src/models/content.model.ts
+++ src/models/content.model.ts
@@ -7,7 +7,7 @@ const contentSchema = new mongoose.Schema({
   type: { type: String, enum: contentTypes, required: true },
   title: { type: String, required: true },
   tags: [{ type: Types.ObjectId, ref: "Tag" }],
-  userId: { type: Types.ObjectId, ref: "User" },
+  userId: { type: Types.ObjectId, ref: "User", required: true },
 });
 
 export const Content = mongoose.model("Content", contentSchema);
