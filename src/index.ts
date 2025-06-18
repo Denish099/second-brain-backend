5d12df3cbf6923d24cbe41bc2a9b74de92170ca6
Denish099 <gyldenish@gmail.com>
2025-06-18 15:22:05 +0530

    added cors validation

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import contentRoutes from "./routes/contentRoutes";
 import shareRoutes from "./routes/shareRoutes";
 import { connectDb } from "./lib/db";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 config();
 const app = express();
@@ -12,6 +13,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 // Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/content", contentRoutes);
