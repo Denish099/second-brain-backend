2224460f03d6c9ec7f3bc713e5a55d837042e463
Denish099 <gyldenish@gmail.com>
2025-06-19 12:14:38 +0530

    added authToken in route of getCurrentUser

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { signup, login, getCurrentUser } from "../controllers/authController";
+import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = Router(); // ✅ should be Router, not Application
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/me", getCurrentUser);
+router.get("/me", authenticateToken, getCurrentUser);
 
 export default router;
